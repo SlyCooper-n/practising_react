648063aae3ebdba1862d6f1f3e584630c44a5453
Gabriel <gabrielvitor.frasao@gmail.com>
2022-05-12 17:30:27 -0300

    header added

--- color-generator/src/App.jsx
+++ color-generator/src/App.jsx
@@ -1,5 +1,47 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="App">Hello, React!</div>;
+  return (
+    <div className="min-h-screen py-12 bg-gray-900 text-slate-100 font-semibold">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
+
+function Header() {
+  const [hexColor, setHexColor] = useState("");
+
+  function setValue(event) {
+    setHexColor(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(hexColor);
+  }
+
+  return (
+    <section className="px-8 flex items-center">
+      <h1 className="mr-8 text-4xl font-bold">Color Generator</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="text"
+            value={hexColor}
+            onChange={setValue}
+            placeholder="Type a hex color. Ex: #ffffff"
+            className="mr-4 px-2 py-1 indent-1 rounded-sm"
+          />
+        </label>
+
+        <button type="submit" className="px-4 py-1 bg-gray-700 rounded-sm">
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+}
