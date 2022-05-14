1049621e50d498b64fd406ad8cd67113f25f5342
Gabriel <gabrielvitor.frasao@gmail.com>
2022-05-14 09:25:08 -0300

    base structure

--- grocery-bud/src/App.jsx
+++ grocery-bud/src/App.jsx
@@ -1,7 +1,24 @@
+import List from "./components/List";
+
 function App() {
   return (
-    <div className="App">
-      <header className="bg-red-500">Hello, React!</header>
+    <div className="min-h-screen flex justify-center items-center bg-slate-200">
+      <main className="p-4 bg-white shadow-2xl text-center text-slate-900 font-semibold">
+        <h1 className="mb-4 text-2xl font-bold">Grocery Bud</h1>
+
+        <form className="flex items-center">
+          <input
+            type="text"
+            className="h-8 mr-2 bg-neutral-200 indent-2 rounded-sm outline-none focus:ring-2 ring-slate-900 ring-offset-2"
+          />
+
+          <button className="h-8 px-2 bg-slate-500 text-slate-50 rounded-sm outline-none focus:ring-2 ring-slate-900 ring-offset-2">
+            Submit
+          </button>
+        </form>
+
+        <List />
+      </main>
     </div>
   );
 }
---
+++ grocery-bud/src/components/List.jsx
@@ -0,0 +1,15 @@
+function components() {
+  return (
+    <div>
+      <ul>
+        <li></li>
+      </ul>
+
+      <button className="w-full bg-red-400 text-red-900 rounded-sm outline-none focus:ring-2 ring-slate-900 ring-offset-2">
+        Clear list
+      </button>
+    </div>
+  );
+}
+
+export default components;
