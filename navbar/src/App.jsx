b906d5acb5b264097ce875c07e308757d66685eb
Gabriel <gabrielvitor.frasao@gmail.com>
2022-05-14 21:17:13 -0300

    navbar finished

--- navbar/src/App.jsx
+++ navbar/src/App.jsx
@@ -20,28 +20,29 @@ function App() {
 function Header() {
   const [showNav, setShowNav] = useState(false),
     linksListRef = useRef(null),
-    navRef = useRef(null);
+    [navHeight, setNavHeight] = useState("");
 
   useEffect(() => {
     let linksListHeight = linksListRef.current.clientHeight;
 
     if (!showNav) {
-      navRef.current.style.height = "0px";
+      setNavHeight("0px");
       return;
     }
-    navRef.current.style.height = linksListHeight + "px";
+
+    setNavHeight(linksListHeight + "px");
   }, [showNav]);
 
   return (
     <header className="w-full py-4 bg-white shadow-lg">
-      <div className="container mx-auto">
+      <div className="container mx-auto px-2 lg:flex lg:justify-between lg:items-center">
         <div className="flex justify-between items-center">
           <img src={logo} alt="logo-ex" />
 
           <button
             type="button"
             onClick={() => setShowNav((prevShowNav) => !prevShowNav)}
-            className="hover:rotate-90 transition-transform"
+            className="lg:hidden hover:rotate-90 transition-transform"
           >
             <FaBars className="text-[1.8rem]" />
           </button>
@@ -51,9 +52,9 @@ function Header() {
           className={`${
             showNav ? "mt-4" : "m-0"
           } overflow-hidden transition-all duration-500`}
-          ref={navRef}
+          style={{ height: navHeight }}
         >
-          <ul className="links text-lg" ref={linksListRef}>
+          <ul className="links text-lg lg:flex" ref={linksListRef}>
             {links.map((link) => (
               <li
                 key={link.id}
@@ -61,44 +62,42 @@ function Header() {
               >
                 <a
                   href={link.url}
-                  className="capitalize group-hover:pl-2 transition-all"
+                  className="capitalize group-hover:pl-2 lg:group-hover:pl-0 transition-all"
                 >
                   {link.text}
                 </a>
               </li>
             ))}
           </ul>
-
-          <ul className="hidden">
-            {social.map((el) => (
-              <li key={el.id} className="mx-2 text-2xl">
-                <a href={el.url}>
-                  {(el.icon == "insta" && (
-                    <FaInstagram className="text-rose-700" />
-                  )) ||
-                    (el.icon == "linkedin" && (
-                      <FaLinkedin className="text-blue-700" />
-                    )) ||
-                    (el.icon == "github" && (
-                      <FaGithub className="text-black" />
-                    )) ||
-                    (el.icon == "twitter" && (
-                      <FaTwitter className="text-sky-500" />
-                    ))}
-                </a>
-              </li>
-            ))}
-          </ul>
         </nav>
 
+        <ul className="hidden lg:flex">
+          {social.map((el) => (
+            <li key={el.id} className="mx-2 text-2xl">
+              <a href={el.url}>
+                {(el.icon == "insta" && (
+                  <FaInstagram className="text-rose-700" />
+                )) ||
+                  (el.icon == "linkedin" && (
+                    <FaLinkedin className="text-blue-700" />
+                  )) ||
+                  (el.icon == "github" && (
+                    <FaGithub className="text-black" />
+                  )) ||
+                  (el.icon == "twitter" && (
+                    <FaTwitter className="text-sky-500" />
+                  ))}
+              </a>
+            </li>
+          ))}
+        </ul>
+
         <style>{`
-        .show-nav {
-          height: fit-content;
-        }
-        .hide-nav {
-          height: 0;
-          margin: 0;
-        }        
+        @media (min-width:1024px) {
+          nav {
+            height: auto !important;
+          }
+        }     
       `}</style>
       </div>
     </header>
