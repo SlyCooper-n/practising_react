2f3be51a020c936fed79313fee59bbde7cb225cb
Gabriel <gabrielvitor.frasao@gmail.com>
2022-05-16 17:42:13 -0300

    data.js added

---
+++ stripe/src/data.js
@@ -0,0 +1,28 @@
+const sublinks = [
+  {
+    page: "products",
+    links: [
+      { label: "payment", icon: "creditCard", url: "/products" },
+      { label: "terminal", icon: "creditCard", url: "/products" },
+      { label: "connect", icon: "creditCard", url: "/products" },
+    ],
+  },
+  {
+    page: "developers",
+    links: [
+      { label: "plugins", icon: "book", url: "/products" },
+      { label: "libraries", icon: "book", url: "/products" },
+      { label: "help", icon: "book", url: "/products" },
+      { label: "billing", icon: "book", url: "/products" },
+    ],
+  },
+  {
+    page: "company",
+    links: [
+      { label: "about", icon: "briefCase", url: "/products" },
+      { label: "customers", icon: "briefCase", url: "/products" },
+    ],
+  },
+];
+
+export default sublinks;
