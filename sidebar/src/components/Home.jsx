95ee5d1dea578f2affa861a05bcb5ec8863459e8
Gabriel <gabrielvitor.frasao@gmail.com>
2022-05-15 21:05:46 -0300

    main components created

---
+++ sidebar/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import { FaBars } from "react-icons/fa";
+
+function Home() {
+  return (
+    <div>
+      <header>
+        <FaBars />
+      </header>
+
+      <section>
+        <button>Show Modal</button>
+      </section>
+    </div>
+  );
+}
+
+export default Home;
