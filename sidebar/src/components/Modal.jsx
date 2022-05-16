6458dd9a07571e8ae6693dd1ced664c8748b34f4
Gabriel <gabrielvitor.frasao@gmail.com>
2022-05-15 21:19:51 -0300

    basic structure

--- sidebar/src/components/Modal.jsx
+++ sidebar/src/components/Modal.jsx
@@ -1,5 +1,13 @@
-import React from "react";
+import { FaTimes } from "react-icons/fa";
 
 export default function Modal() {
-  return <div>Modal</div>;
+  return (
+    <div className="hidden">
+      <button>
+        <FaTimes />
+      </button>
+
+      <section>Modal content</section>
+    </div>
+  );
 }
--- sidebar/src/components/Sidebar.jsx
+++ sidebar/src/components/Sidebar.jsx
@@ -1,5 +1,44 @@
-import React from "react";
+import logo from "../logo.svg";
+import {
+  FaTimes,
+  FaBehance,
+  FaFacebook,
+  FaLinkedin,
+  FaTwitter,
+  FaSketch,
+  FaHome,
+  FaUserFriends,
+  FaFolderOpen,
+  FaCalendarAlt,
+  FaWpforms,
+} from "react-icons/fa";
+import { links } from "../data";
 
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <aside className="hidden">
+      <div>
+        <img src={logo} alt="logo" />
+
+        <button>
+          <FaTimes />
+        </button>
+      </div>
+
+      <ul>
+        {links.map((link) => (
+          <li key={link.id}>
+            <a href={link.url}>
+              {link.icon == "home" && <FaHome />}
+              {link.icon == "userfriends" && <FaUserFriends />}
+              {link.icon == "folderOpen" && <FaFolderOpen />}
+              {link.icon == "calendarAlt" && <FaCalendarAlt />}
+              {link.icon == "wpForms" && <FaWpforms />}
+              {link.text}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
