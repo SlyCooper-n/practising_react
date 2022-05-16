cd6331ed6ed9c9ce37be4764757f01d9bd6f3273
Gabriel <gabrielvitor.frasao@gmail.com>
2022-05-15 21:19:07 -0300

    jsx fixed

--- sidebar/src/data.js
+++ sidebar/src/data.js
@@ -1,46 +1,33 @@
-import React from "react";
-import {
-  FaBehance,
-  FaFacebook,
-  FaLinkedin,
-  FaTwitter,
-  FaSketch,
-  FaHome,
-  FaUserFriends,
-  FaFolderOpen,
-  FaCalendarAlt,
-  FaWpforms,
-} from "react-icons/fa";
 export const links = [
   {
     id: 1,
     url: "/",
     text: "home",
-    icon: <FaHome />,
+    icon: "home",
   },
   {
     id: 2,
     url: "/team",
     text: "team",
-    icon: <FaUserFriends />,
+    icon: "userFriends",
   },
   {
     id: 3,
     url: "/projects",
     text: "projects",
-    icon: <FaFolderOpen />,
+    icon: "folderOpen",
   },
   {
     id: 4,
     url: "/calendar",
     text: "calendar",
-    icon: <FaCalendarAlt />,
+    icon: "calendarAlt",
   },
   {
     id: 5,
     url: "/documents",
     text: "documents",
-    icon: <FaWpforms />,
+    icon: "wpForms",
   },
 ];
 
@@ -48,26 +35,26 @@ export const social = [
   {
     id: 1,
     url: "https://www.twitter.com",
-    icon: <FaFacebook />,
+    icon: "facebook",
   },
   {
     id: 2,
     url: "https://www.twitter.com",
-    icon: <FaTwitter />,
+    icon: "twitter",
   },
   {
     id: 3,
     url: "https://www.twitter.com",
-    icon: <FaLinkedin />,
+    icon: "linkedin",
   },
   {
     id: 4,
     url: "https://www.twitter.com",
-    icon: <FaBehance />,
+    icon: "behance",
   },
   {
     id: 5,
     url: "https://www.twitter.com",
-    icon: <FaSketch />,
+    icon: "sketch",
   },
 ];
