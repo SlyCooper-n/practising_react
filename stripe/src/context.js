ef9d141ad7517e56005c40f3c0fed1f7305c626f
Gabriel <gabrielvitor.frasao@gmail.com>
2022-05-16 19:17:14 -0300

    context file renamed

--- stripe/src/context.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AppContext = React.createContext(null);
-
-export function AppProvider(children) {
-  return <AppContext.Provider>{children}</AppContext.Provider>;
-}
---
+++ stripe/src/context.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+export const AppContext = React.createContext(null);
+
+export function AppProvider({ children }) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false),
+    [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+
+  function toggleSidebar() {
+    setIsSidebarOpen((prev) => !prev);
+  }
+
+  function toggleSubmenu() {
+    setIsSubmenuOpen((prev) => !prev);
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        sidebar: { isSidebarOpen, toggleSidebar },
+        submenu: { isSubmenuOpen, toggleSubmenu },
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+export function useGlobalContext() {
+  return useContext(AppContext);
+}
