f9bf941426332113ac1904f9b8c96f63f1c2104e
Gabriel <gabrielvitor.frasao@gmail.com>
2022-05-19 19:54:43 -0300

    navbar margin for mobile

--- cocktails/src/components/layout/Header/index.jsx
+++ cocktails/src/components/layout/Header/index.jsx
@@ -1,10 +1,10 @@
 export default function () {
   return (
     <header className="mb-12 py-6 bg-white text-green-800 text-xl font-semibold border-4 border-b-green-900 shadow-xl">
-      <div className="container mx-auto px-1 flex justify-between items-center">
+      <div className="container mx-auto px-1 flex flex-col sm:flex-row justify-between items-center">
         <h1 className="text-3xl font-mono">TheCocktailDB</h1>
 
-        <nav>
+        <nav className="mt-8 sm:mt-0">
           <ul className="flex gap-8">
             <li>
               <a href="#">Home</a>
