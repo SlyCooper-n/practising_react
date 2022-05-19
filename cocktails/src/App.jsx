edf1a16bb20b5fde90845361e6c5576232f36f86
Gabriel <gabrielvitor.frasao@gmail.com>
2022-05-19 19:53:27 -0300

    base structure

--- cocktails/src/App.jsx
+++ cocktails/src/App.jsx
@@ -1,7 +1,17 @@
+import CocktailsList from "./components/layout/CocktailsList";
+import Header from "./components/layout/Header";
+import SearchField from "./components/layout/SearchField";
+
 function App() {
   return (
-    <div className="">
-      <header className=""></header>
+    <div className="min-h-screen bg-slate-100">
+      <Header />
+
+      <main className="flex flex-col items-center font-semibold">
+        <SearchField />
+
+        <CocktailsList />
+      </main>
     </div>
   );
 }
